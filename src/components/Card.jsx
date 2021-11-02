2ba8b579833596f64de2a48c8e1606f1e16dcb72
elecalderon1501 <elecalderon.ec@gmail.com>
2021-11-02 09:48:07 -0300

    Second push

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,11 +6,11 @@ export default function Card ({min, max, name, img, onClose, id}) {
     return (
       <div className = {estilos.contenedor}>
       <button className= {estilos.btn} onClick = {onClose}>X</button>
-      <h4>{name}</h4>      
-      <h6>Min</h6>
-      <p>{min}</p>     
-      <h6>Max</h6>
-      <p>{max}</p>
+      <div><h4>{name}</h4></div>      
+      <div><h6>Min</h6></div>
+      <div><p>{min}</p></div> 
+      <div><h6>Max</h6></div>    
+      <div><p>{max}</p></div>
       
       <img src={ `http://openweathermap.org/img/wn/${img}@2x.png` } />
     </div> 
